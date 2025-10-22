6167f02bce015debe074aa6634dce8426910c38d
leoferamos <fernandesramosleonardo@gmail.com>
2025-10-22 17:00:32 -0300

    feat(payment-service): initialize payment service with Maven setup and application structure

---
+++ services/payment-service-java/src/main/java/io/github/leoferamos/grpc/payment_service_java/PaymentServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.leoferamos.grpc.payment_service_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceJavaApplication.class, args);
+	}
+
+}
---
+++ services/payment-service-java/src/test/java/io/github/leoferamos/grpc/payment_service_java/PaymentServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leoferamos.grpc.payment_service_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
